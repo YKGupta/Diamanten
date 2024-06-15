81bdb39d7cd9cd7bddf2c7155e4ac743bd4f6eb9
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-06-15 23:16:23 +0530

    Integrated animations with collection and set parenting of lamp appropriately

--- Assets/Scripts/Flashlight/Lamp.cs
+++ Assets/Scripts/Flashlight/Lamp.cs
@@ -1,10 +1,18 @@
+using System.Collections;
 using UnityEngine;
 using NaughtyAttributes;
+using UnityEngine.Animations.Rigging;
 
 public class Lamp : MonoBehaviour, IInteractionEffect
 {
     [BoxGroup("Settings")]
     public Flashlight flashlight;
+    [BoxGroup("Settings")]
+    public float moveHandToLampDuration = 1f;
+    [BoxGroup("Settings")]
+    public TwoBoneIKConstraint handIK;
+    [BoxGroup("Settings")]
+    public Animator fingersAnimator;
     [BoxGroup("Interaction Settings")]
     [Range(0f, 10f)]
     public float range = 2f;
@@ -23,10 +31,31 @@ public class Lamp : MonoBehaviour, IInteractionEffect
 
     public void OnCollect(GameObject obj)
     {
+        if(!enabled)
+            return;
+
+        enabled = false;
+        PlayerManager.instance.isPlayerAllowedToMove = false;
+        PlayerManager.instance.isPlayerAllowedToCrouch = false;
         flashlight.useLight = true;
-        gameObject.SetActive(false);
+        TimerInstance timer = Timer.instance.CreateTimer(moveHandToLampDuration, 1, "hand");
+        timer.timerStart += MoveHand;
     }
 
+    public IEnumerator MoveHand(TimerInstance timer)
+    {
+        while(!Mathf.Approximately(timer.NormalizedValue(), 1f))
+        {
+            handIK.weight = timer.NormalizedValue();
+            yield return null;
+        }
+        
+        handIK.weight = 1f;
+        fingersAnimator.SetTrigger("hold");
+        transform.SetParent(handIK.transform);
+        PlayerManager.instance.isPlayerAllowedToMove = true;
+        PlayerManager.instance.isPlayerAllowedToCrouch = true;
+    }
     public void StartEffect()
     {
         interactionUIGO.SetActive(true);
